3783021ebda2a47bf18ddb23c91e01c3798e0f39
Cao Nguyen <cao.nguyen@base.vn>
2020-08-19 09:52:17 +0700

    Add settingsReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,13 @@ import { devicesReducer, setDevicesStore } from "./devices";
 import { setStore } from "./storeAccess";
 import { recordsReducer } from "./records";
 import { pushedRecordsReducer } from "./pushedRecords";
+import { settingsReducer } from "./settings";
 
 const rootReducer = combineReducers({
   devices: devicesReducer,
   records: recordsReducer,
   pushedRecords: pushedRecordsReducer,
+  settings: settingsReducer
 });
 
 export const store = createStore(rootReducer);
---
+++ src/store/settings/index.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { getStore } from "../storeAccess";
+
+const initialState: Record<string, any> = {};
+
+const { actions, reducer } = createSlice({
+  name: "settings",
+  initialState,
+  reducers: {
+    setSetting(state, action: PayloadAction<{ key: string; value: any }>) {
+      state[action.payload.key] = action.payload.value;
+    },
+  },
+});
+
+const setSetting = (key: string, value: any) => {
+  return getStore().dispatch(actions.setSetting({ key, value }));
+};
+
+export const createSetting = <T>(key: string, defaultValue: T) => {
+  const set = (value: T) => setSetting(key, value);
+  const get = () => getStore().getState().settings[key] || defaultValue;
+
+  return {
+    get,
+    set,
+  };
+};
+
+export {
+  reducer as settingsReducer
+}
