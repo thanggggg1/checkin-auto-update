e4e42597f0a90878506c070a4917a3085c9e7302
Cao Nguyen <cao.nguyen@base.vn>
2020-08-21 14:30:44 +0700

    Update connections

--- src/components/DevicesRow/context.ts
+++ src/components/DevicesRow/context.ts
@@ -2,13 +2,15 @@ import constate from "constate";
 import { deleteDevices, Device } from "../../store/devices";
 import ZK from "../../packages/js_zklib/ZK";
 import { useCallback, useEffect, useState } from "react";
-import { useAsyncFn, useUpdateEffect } from "react-use";
+import { useAsync, useAsyncFn, useLatest, useUpdateEffect } from "react-use";
 import {
   formatRawAttendanceRecords,
   syncAttendanceRecords,
 } from "../../store/records";
 import useAutoAlertError from "../../hooks/useAutoAlertError";
 import { Events, events } from "../../utils/events";
+import { setIntervalAsync } from "set-interval-async/dynamic";
+import { clearIntervalAsync } from "set-interval-async";
 
 export enum SyncState {
   NOT_STARTED,
@@ -24,13 +26,14 @@ export enum ConnectionState {
 
 const useDeviceValue = ({ device }: { device: Device }) => {
   const [state, setState] = useState(ConnectionState.PENDING);
+  const latestState = useLatest(state);
   const [syncState, setSyncState] = useState<SyncState>(SyncState.NOT_STARTED);
   const [realtimeState, setRealtimeState] = useState("Pending");
   const [connection, setConnection] = useState<ZK>(() => {
     return new ZK({
       port: device.port,
       connectionType: device.connection,
-      timeout: 10000,
+      timeout: 3000,
       inport: 5200,
       ip: device.ip,
     });
@@ -41,18 +44,17 @@ const useDeviceValue = ({ device }: { device: Device }) => {
       new ZK({
         port: device.port,
         connectionType: device.connection,
-        timeout: 5000,
+        timeout: 3000,
         inport: 5200,
         ip: device.ip,
       })
     );
-  }, [device]);
+  }, [device.ip, device.port, device.connection]);
 
   useEffect(() => {
     let interval = 0;
-    connection.connect().then(async () => {
-      setState(ConnectionState.CONNECTED);
 
+    connection.createSocket().then(() => {
       // @todo clear
       // @ts-ignore
       connection.zklib.socket.on("close", () => {
@@ -60,37 +62,52 @@ const useDeviceValue = ({ device }: { device: Device }) => {
         setRealtimeState("Closed");
       });
 
-      setRealtimeState("Pending");
-      interval = setInterval(() => {
-        connection.startMon({
-          start: (err) => {
-            if (err) return setRealtimeState("Timed out");
-            setRealtimeState("Started");
-          },
-          onatt: (log) => {
-            console.log("onatt", log);
-          },
-        });
-      }, 3000);
+      // @ts-ignore
+      connection.zklib.socket.on("connection", () => {
+        setState(ConnectionState.CONNECTED);
+      });
+
+      connection.connect().then(async () => {
+        setState(ConnectionState.CONNECTED);
+        setRealtimeState("Pending");
+
+        interval = setInterval(() => {
+          connection.startMon({
+            start: (err) => {
+              if (err) return setRealtimeState("Timed out");
+              setRealtimeState("Started");
+            },
+            onatt: (log) => {
+              console.log("onatt", log);
+            },
+          });
+        }, 3000);
+      });
     });
 
     return () => {
       clearInterval(interval);
       connection.disconnect();
     };
-  }, [connection]);
+  }, []);
 
   useEffect(() => {
-    let interval = 0;
-    if (state !== ConnectionState.CONNECTED) {
-      interval = setInterval(() => {
-        connection.connect();
-      }, 3000);
-    }
+    const interval = setIntervalAsync(async () => {
+      if (latestState.current !== ConnectionState.CONNECTED) {
+        try {
+          console.log("trying to connect", device.ip);
+          await connection.connect();
+          setState(ConnectionState.CONNECTED);
+        } catch (e) {
+          console.log("connect error", e);
+          setState(ConnectionState.PENDING);
+        }
+      }
+    }, 3000);
     return () => {
-      clearInterval(interval);
+      clearIntervalAsync(interval);
     };
-  }, [state]);
+  }, []);
 
   const [{ error }, syncAttendances] = useAsyncFn(async () => {
     if (state !== ConnectionState.CONNECTED) return;
@@ -108,6 +125,24 @@ const useDeviceValue = ({ device }: { device: Device }) => {
   }, [connection, state, device]);
 
   useAutoAlertError(error);
+
+  // Update free space continuously
+  useAsync(async () => {
+    const interval = setIntervalAsync(async () => {
+      try {
+        if (latestState.current !== ConnectionState.CONNECTED) return;
+        console.log("start getting freesizes", device.ip);
+        const freeSizes = await connection.getFreeSizes();
+        console.log("freeSizes", freeSizes);
+      } catch (e) {
+        setState(ConnectionState.PENDING);
+      }
+    }, 5000);
+
+    return () => {
+      clearIntervalAsync(interval);
+    };
+  }, [connection]);
 
   useEffect(() => {
     events.on(Events.MASS_SYNC, syncAttendances);
