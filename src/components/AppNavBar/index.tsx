4d935be8f2130302081cc28d4f0d6e82587961c7
caonguyen <cao.nguyen@base.vn>
2020-09-04 09:41:54 +0700

    Add App Version to Title

--- src/components/AppNavBar/index.tsx
+++ src/components/AppNavBar/index.tsx
@@ -7,23 +7,31 @@ import LogoutButton from "./components/LogoutButton";
 import PushButton from "./components/PushButton";
 import ButtonGroup from "antd/lib/button/button-group";
 import SyncButton from "./components/SyncButton";
-import ClearRecordsButton from "./components/ClearRecordsButton";
 
 const Wrapper = styled(Row)`
   padding: 0 16px;
   height: 56px;
 `;
 
+const AppNameWrapper = styled(Row)`
+  align-items: baseline;
+`;
+
 const AppName = styled.h1`
   font-size: 20px;
-  margin: 0;
+  margin: 0 8px 0 0;
 `;
+
+
 const AppNavBar = memo(function AppNavBar() {
   const token = Fetch.useToken();
 
   return (
     <Wrapper align={"middle"} justify={"space-between"}>
-      <AppName>Base Checkin Station</AppName>
+      <AppNameWrapper>
+        <AppName>Base Checkin Station</AppName>
+        <span>v{require('electron').remote.app.getVersion()}</span>
+      </AppNameWrapper>
 
       <Row>
         <ButtonGroup>
