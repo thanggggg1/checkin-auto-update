940a2510168404d6d184fcfa5755b3db71a2ee16
dinh.nguyen <dinh.nguyen@base.vn>
2022-09-28 09:06:05 +0700

    as

--- src/components/AddDeviceModal/index.tsx
+++ src/components/AddDeviceModal/index.tsx
@@ -24,7 +24,7 @@ const defaultValue: Device = {
   username: "", //"admin",
   status: "Online",
   token: "",
-  syncMethod: "",
+  syncMethod: DeviceSyncMethod.PY,
   connection: "tcp",
   ip: ""
 };
